b7e8ae668ef9bdb2824d6410f4ff7199e2d9b0ea
Pardocks <pardocks12700@gmail.com>
2025-10-25 20:49:27 +1000

    make chat not indefinitely spam

--- chat/chat.gd
+++ chat/chat.gd
@@ -4,17 +4,26 @@ extends Control
 @onready var chat_messages: TextEdit = $MarginContainer/VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/VBoxContainer/Messages
 @onready var chat_messager: LineEdit = $MarginContainer/VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Messager
 
-var finished: String
+var last_message_count := 0
 
 func _ready() -> void:
 	chat_player_name.text = GlobalData.username
+	chat_messages.editable = false  # Prevent user editing
 
 func _on_messager_text_submitted(new_text: String) -> void:
 	GlobalData.send_chat_message(new_text)
 	chat_messager.text = ""
 
 func _process(_delta: float) -> void:
+	if GlobalData.chat_messages.size() != last_message_count:
+		update_chat_display()
+		last_message_count = GlobalData.chat_messages.size()
+
+func update_chat_display() -> void:
+	var text := ""
 	for chat in GlobalData.chat_messages:
-		finished += "[%s]: %s" % [chat["user"], chat["msg"]] + "\n"
-	chat_messages.text = finished
-	await get_tree().create_timer(5).timeout
+		text += "[%s]: %s\n" % [chat["user"], chat["msg"]]
+	
+	chat_messages.text = text
+	
+	chat_messages.scroll_vertical = chat_messages.get_v_scroll_bar().max_value - 2
