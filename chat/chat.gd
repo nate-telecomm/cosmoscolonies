dfe9c634ee6b39da181222b4134c70e87ea69a05
Pardocks <pardocks12700@gmail.com>
2025-10-25 18:21:33 +1000

    chat operation has been moved to chat.gd

--- chat/chat.gd
+++ chat/chat.gd
@@ -1,16 +1,19 @@
 extends Control
 
-@onready var chat_player_name: Button = $MarginContainer/VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/PlayerName
+@onready var chat_player_name: RichTextLabel = $MarginContainer/VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/PlayerName
 @onready var chat_messages: TextEdit = $MarginContainer/VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/VBoxContainer/Messages
 @onready var chat_messager: LineEdit = $MarginContainer/VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Messager
+
+var finished: String
 
 func _ready() -> void:
 	chat_player_name.text = GlobalData.username
 
 func _on_messager_text_submitted(new_text: String) -> void:
-	## This is where network code is need to send the message to the server
-	pass
+	GlobalData.send_chat_message(new_text)
+	chat_messager.text = ""
 
-func _on_message(username: String, text: String):
-	chat_messages.text += str(username + ": " + text)
-	## gotta somehow get this to update across all clients 
+func _process(_delta: float) -> void:
+	for chat in GlobalData.chat_messages:
+		finished += "[%s]: %s" % [chat["user"], chat["msg"]] + "\n"
+	chat_messages.text = finished
