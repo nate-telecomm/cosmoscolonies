551f082ecf4399f836fd51734bb7de34404adaff
Pardocks <pardocks12700@gmail.com>
2025-10-25 15:13:47 +1000

    chat added to player and obtains username

--- chat/chat.gd
+++ chat/chat.gd
@@ -5,9 +5,7 @@ extends Control
 @onready var chat_messager: LineEdit = $MarginContainer/VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Messager
 
 func _ready() -> void:
-	#chat_player_name = <username> 
-	## Idfk how the network code works so someones gotta get the username on the chat_player_name variable
-	pass
+	chat_player_name.text = GlobalData.username
 
 func _on_messager_text_submitted(new_text: String) -> void:
 	## This is where network code is need to send the message to the server
